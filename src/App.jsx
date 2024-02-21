36955841d81c40cde0ef758b1f4d64091652216d
devxshubham <devshubhamcode@gmail.com>
2024-02-21 23:11:47 +0530

    added sidebar

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { RevenueCard } from "./components/revenueCard"
 import { BlueRevenueCard } from "./components/BlueRevenueCard"
 import { Navbar } from "./components/navbar"
 import { Content } from "./components/Content"
+import { SideBar } from "./components/sidebar"
 
 
 function App() {
@@ -9,10 +10,13 @@ function App() {
 
   return (
     <>
-      <div className="">
-          <Navbar></Navbar>
-          <Content></Content>
-          <div className="text-center text-gray-500 m-5">Made by <a className="underline font-bold " target="_blank" href="https://twitter.com/devxshubham">Shubham Dev</a></div>
+      <div className="flex">
+          <SideBar></SideBar>
+          <div>
+            <Navbar></Navbar>
+            <Content></Content>
+            <div className="text-center text-gray-500 m-5">Made by <a className="underline font-bold " target="_blank" href="https://twitter.com/devxshubham">Shubham Dev</a></div>
+          </div>
       </div>
     </>
   )
--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,6 +1,11 @@
 export const Navbar = () => {
     return <header className="flex justify-between px-8 py-3 border-b">
-        <div className="flex items-center  gap-2">
+        <div className="flex items-center  gap-3">
+            <button className=" lg:hidden">
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
+                <path strokeLinecap="round" strokeLinejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5" />
+            </svg>
+            </button>
             <h5 className=" font-medium text-[#1A181E]">Payouts</h5>
             <div className="flex items-center text-xs text-[#4D4D4D]">
                 <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-4 h-4">
