a486445a5eed41eef019bb3386618ba86a2d32d3
devxshubham <devshubhamcode@gmail.com>
2024-02-27 22:01:17 +0530

    atom changed

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -9,7 +9,7 @@ export const Navbar = () => {
             <button className=" lg:hidden">
             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6"
              onClick={() => {
-                setSide( prev => prev+1);
+                (side==true) ? setSide(false) : setSide(true) ;
              }}>
                 <path strokeLinecap="round" strokeLinejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5" />
             </svg>
--- src/store/atom/sideAtom.jsx
+++ src/store/atom/sideAtom.jsx
@@ -2,5 +2,5 @@ import {atom} from 'recoil'
 
 export const sideAtom = atom({
     key : 'sideAtom',
-    default : 0
+    default : true
 })
