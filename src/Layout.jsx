0e485631f39e4c759d19392a4a38166fb543ab77
devxshubham <devshubhamcode@gmail.com>
2024-02-29 00:14:48 +0530

    resolved error

--- src/Layout.jsx
+++ src/Layout.jsx
@@ -1,8 +1,18 @@
 import { Navbar } from "./components/navbar"
 import { Content } from "./components/Content"
 
+import { isOpenAtom } from "./store/atom/isOpenAtom"
+import { useRecoilValue } from "recoil"
+
 export const Layout = ()=>{
-    return <div >
+    const isOpen = useRecoilValue(isOpenAtom)
+
+    return <div>
+        { isOpen 
+            ?<div className="absolute inset-0 transition-opacity bg-gray-500 bg-opacity-75"></div>
+            : <div></div>
+        }
+
         <Navbar></Navbar>
         <Content></Content>
         <div className="text-center text-gray-500 m-5">Made by <a className="underline font-bold " target="_blank" href="https://twitter.com/devxshubham">Shubham Dev</a></div>
